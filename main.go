60235de2f916084fd9a0f00a3b07bfdf8d431d5f
lucasalustiano <lucas.salustiano@ccc.ufcg.edu.br>
2021-08-09 20:42:12 -0300

    UC1 - done

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "bufio"
+    "net/http"
+)
+
+func main() {
+
+	urlptr := flag.String("url", "http://google.com", "url to be crawled")
+
+    flag.Parse()
+
+    //fmt.Println("url:", *urlptr)
+    resp, err := http.Get(*urlptr)
+    if err != nil {
+	panic(err)
+    }
+
+    defer resp.Body.Close()
+
+    fmt.Println("Resposnse Status", resp.Status)
+
+    scanner := bufio.NewScanner(resp.Body)
+    for i := 0; scanner.Scan(); i++ {
+	fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+	panic(err)
+    }
+}
